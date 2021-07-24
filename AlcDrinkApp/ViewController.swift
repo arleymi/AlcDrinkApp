38be32c959618c47485f89571de89206c2d55b09
Arthur <arthur.lee290499@gmail.com>
2021-07-24 17:17:36 +0600

    Initial Commit

---
+++ AlcDrinkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlcDrinkApp
+//
+//  Created by Arthur Lee on 24.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
